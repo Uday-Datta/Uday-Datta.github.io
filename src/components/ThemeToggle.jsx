f01488c66b31b5661871380576241b584d41743e
Uday Datta <udaydatta79@gmail.com>
2025-08-14 16:04:11 +0600

    toggle button updated to centered

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,7 +32,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-4 right-10 z-50 p-2 rounded-full transition-colors duration-300",
+        "fixed top-4 left-1/2 -translate-x-1/2 sm:left-auto sm:right-10 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-hidden cursor-pointer"
       )}
     >
